3c5810b556cae4c7db86559e004297cc761a418f
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2026-09-08 14:39:49 +0530

    Update user.js

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { getDatabase } from "../config/db.js";
 
 const USERS_COLLECTION = "users";
@@ -20,19 +21,36 @@ export async function ensureUserIndexes() {
 
 export function createUserDocument({
   username,
+
+  // Server-side password authentication data
   passwordHash,
+  passwordHashSalt,
+  passwordHashKdfParams,
+
+  // Client-side Master Key protection data
   passwordKdfSalt,
   passwordKdfParams,
+
+  // Protected Master Key
   wrappedMasterKey,
   masterKeyIV,
   masterKeyVersion,
+
+  // ML-KEM-768 key material
   mlKemPublicKey,
   wrappedMlKemPrivateKey,
   privateKeyIV,
 }) {
   return {
     username,
+
+    // Used by the server to verify the user's password.
     passwordHash,
+    passwordHashSalt,
+    passwordHashKdfParams,
+
+    // Used by the browser to derive the KEK
+    // required to unwrap the Master Key.
     passwordKdfSalt,
     passwordKdfParams,
 
@@ -53,7 +71,17 @@ export async function findUserByUsername(username) {
 }
 
 export async function findUserById(userId) {
-  return getUsersCollection().findOne({ _id: userId });
+  let objectId;
+
+  try {
+    objectId = new ObjectId(userId);
+  } catch {
+    return null;
+  }
+
+  return getUsersCollection().findOne({
+    _id: objectId,
+  });
 }
 
 export async function insertUser(userDocument) {
