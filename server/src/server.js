f554d1a8cc672633a0aa226556f36f94b01ead90
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2026-09-08 14:18:36 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,15 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 import { connectDatabase } from "./config/db.js";
+import { ensureUserIndexes } from "./models/user.js";
 
 async function startServer() {
   try {
     await connectDatabase();
+
+    await ensureUserIndexes();
+
+    console.log("Database indexes verified.");
 
     app.listen(env.port, "0.0.0.0", () => {
       console.log(`Server listening on port ${env.port}`);
