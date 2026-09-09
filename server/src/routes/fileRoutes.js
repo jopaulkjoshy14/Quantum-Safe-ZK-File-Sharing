ad0a93897bb574421a2c232c2193190bbf8b4a0d
Jopaul K Joshy <jopaulkjoshy14@gmail.com>
2026-09-09 09:59:10 +0530

    Update fileRoutes.js

--- server/src/routes/fileRoutes.js
+++ server/src/routes/fileRoutes.js
@@ -2,19 +2,19 @@ import express from "express";
 
 import {
   uploadFile,
+  downloadFile,
 } from "../controllers/fileController.js";
 
 const router = express.Router();
 
-/**
- * Upload an already-encrypted file.
- *
- * The frontend encrypts the file before
- * sending it to this endpoint.
- */
 router.post(
   "/upload",
   uploadFile
+);
+
+router.get(
+  "/:fileId/download",
+  downloadFile
 );
 
 export default router;
